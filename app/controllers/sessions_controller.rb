e32f95ccc3f3a19387d141779e037f42f68fb6b3
laservoid <hubertkopij@gmail.com>
2019-03-04 18:17:32 +0100

    add some gemfiles, add user and messages association

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
 
-def login
+  def login
 
-end
+  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(username: "Hubert", password: "password")
+User.create(username: "Jan", password: "password")
+User.create(username: "Baltazar", password: "password")
+User.create(username: "Wieńczysław", password: "password")
+User.create(username: "Burak", password: "password")
+User.create(username: "Onufry", password: "password")
